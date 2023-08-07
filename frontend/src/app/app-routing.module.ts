67c8ff12531b16199d9a95f8a91e87044f68067f
Azrod42 <tom.sorabella@gmail.com>
2023-08-07 15:19:23 +0200

    add_fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from '../app/login/login.component'
+import {LoginComponent} from './login/login.component'
 import {HomeComponent} from "./home/home.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {HomeComponentD} from "./dashboard/home/home.component";
